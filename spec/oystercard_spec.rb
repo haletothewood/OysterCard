2276e386f012f1234f0cb8df00d84325c19cd7f2
David Halewood <davidnhalewood@hotmail.com>
2017-10-03 12:33:01 +0100

    refactoring

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,7 +10,8 @@ describe OysterCard do
   describe '#top_up' do
     it 'tops up balance' do
       previous_balance = subject.balance
-      expect(subject.top_up(10)).to eq previous_balance + 10
+      #expect(subject.top_up(10)).to eq previous_balance + 10
+      expect { subject.top_up(1) }.to change { subject.balance }.by 1
     end
     it 'wont add more than the max balance' do
       subject.top_up(OysterCard::MAX_BALANCE)
